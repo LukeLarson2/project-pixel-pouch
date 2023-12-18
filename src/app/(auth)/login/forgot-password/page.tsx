cf336e97f6b21973507f76456c1115b6973a796a
LukeLarson2 <lucas.m.larson2@gmail.com>
2023-12-18 09:15:10 -0800

    built forgot password page

---
+++ src/app/(auth)/login/forgot-password/page.tsx
@@ -0,0 +1,62 @@
+'use client';
+import { createClientComponentClient } from "@supabase/auth-helpers-nextjs";
+import { useRouter } from 'next/navigation';
+import { useState, useEffect } from 'react';
+
+const companyImage = '/assets/images/pixel-sky-design-logo-small.png';
+
+export default function ForgotPassword() {
+  const [email, setEmail] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const router = useRouter();
+
+  const supabase = createClientComponentClient();
+
+  const handleRequest = async () => {
+    setIsLoading(true);
+    const {error} = await supabase.auth.resetPasswordForEmail(email, {
+      redirectTo: 'http://localhost:3000/account-settings/password-reset'
+    })
+
+    if (error) {
+      console.error(error)
+      setIsLoading(false);
+      return;
+    }
+    setIsLoading(false);
+  }
+
+  return (
+    <div className="login-main-container">
+      {isLoading && (
+        <div className="loader-container">
+        <span className="loader"></span>
+      </div>
+      )}
+      <div className="logo-login">
+        <div className="company-logo" style={{backgroundImage: `url(${companyImage})`}} />
+      </div>
+      <div className="login-modal" style={{ height: '200px' }}>
+        <h2>Forgot Password</h2>
+        <input
+          className="login-field"
+          type="email"
+          name="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          placeholder="example@email.com"
+        />
+
+        <button
+        type='button'
+        className="login-sign-in-btn"
+        onClick={handleRequest}
+        style={{fontSize: '18px'}}
+        >
+          Reset Password
+        </button>
+      </div>
+    </div>
+  )
+}
