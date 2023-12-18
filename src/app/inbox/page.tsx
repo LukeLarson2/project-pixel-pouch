6a220d21d0f4833b350e4206c4e4762479def4c6
LukeLarson2 <lucas.m.larson2@gmail.com>
2023-12-18 11:13:34 -0800

    created barebone for inbox of all messages

---
+++ src/app/inbox/page.tsx
@@ -0,0 +1,89 @@
+'use client';
+
+import { createClientComponentClient } from '@supabase/auth-helpers-nextjs';
+import {useRouter} from 'next/navigation';
+import {useState, useEffect} from 'react';
+
+import { FaCheck } from "react-icons/fa";
+
+import getUser from '../_utils/getUser';
+import formatTimeAgo from '../_utils/formatTimeAgo';
+import truncateString from '../_utils/truncateString';
+
+import './style.css';
+
+type MessageItem = {
+  todo_id: number;
+  user_id: number;
+  project: number;
+  dir_id: number;
+  title: string;
+  message: string;
+  viewed: boolean;
+  complete: boolean;
+  date_added: string;
+  storage_url: string;
+  download: boolean;
+  upload: boolean;
+};
+
+type User = {
+  client_id: string;
+  username: string;
+  email: string;
+  admin: boolean;
+  subscription: string;
+};
+
+export default function Inbox() {
+  const [messages, setMessages] = useState<MessageItem[] | null>(null);
+
+  const supabase = createClientComponentClient();
+  const router = useRouter();
+
+  const getMessages = async () => {
+    const result = await getUser();
+    if (result && Array.isArray(result) && 'client_id' in result[0]) {
+      const user: User = result [0];
+
+      const {data, error} = await supabase
+        .from('messages')
+        .select()
+        .eq('user_id', user.client_id)
+        .order('date_added', {ascending: false});
+
+        if(error) {
+          console.error(error);
+          return;
+        }
+        setMessages(data);
+    }
+  }
+
+  const handleGoToMessage = (id:string) => {
+    router.push(`/inbox/${id}`)
+  }
+
+  useEffect(() => {
+    getMessages();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [supabase])
+
+  return messages && (
+    <div className="all-messages">
+      <h2>Inbox</h2>
+      {messages.map((message) => {
+        const date = formatTimeAgo(message.date_added)
+        const formMessage = truncateString(message.message, 100);
+        return (
+          <div className='singlemessage-container-all' key={`${message.todo_id}`} onClick={() => handleGoToMessage(`${message.todo_id}`)}>
+            {message.complete && <div className="message-completed"><FaCheck /> Completed</div>}
+            <p className="message-time-ago-all">Sent {date}</p>
+            <h3 className="message-title-all">{message.title}</h3>
+            <p className="message-message-all">{formMessage}</p>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
